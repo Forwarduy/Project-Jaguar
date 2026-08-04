5c141193f6fc7cdeedf71fce4926b9ce6b20a220
Guzmán Rodríguez Ippoliti <forwarduruguay@gmail.com>
2026-08-04 16:23:02 -0300

    Delete tests/test directory

--- tests/test/registry.py
+++
@@ -1,8 +0,0 @@
-from agents.registry import AGENT_REGISTRY
-from agents.research import ResearchAgent
-
-def test_registry_has_all_three_commands():
-    assert set(AGENT_REGISTRY.keys()) == {"research", "planning", "outreach"}
-
-def test_registry_maps_to_correct_classes():
-    assert AGENT_REGISTRY["research"] is ResearchAgent
