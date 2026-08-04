d97cd12f5474384a15d0b549bdab902661d585dd
Guzmán Rodríguez Ippoliti <forwarduruguay@gmail.com>
2026-08-04 16:20:51 -0300

    Delete tests/test_registry.py

--- tests/test_registry.py
+++
@@ -1,8 +0,0 @@
-def test_registry_has_all_three_commands():
-    assert set(AGENT_REGISTRY.keys()) == {"research", "plan", "planning", "outreach"}
-
-def test_registry_maps_to_correct_classes():
-    assert AGENT_REGISTRY["research"] is ResearchAgent
-    assert AGENT_REGISTRY["plan"] is PlanningAgent
-    assert AGENT_REGISTRY["planning"] is PlanningAgent  # alias
-    assert AGENT_REGISTRY["outreach"] is OutreachAgent
