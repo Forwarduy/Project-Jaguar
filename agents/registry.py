a0ec3fd05a882e668d92b3579624636d933ea6a1
Guzmán Rodríguez Ippoliti <forwarduruguay@gmail.com>
2026-08-04 11:22:36 -0300

    Delete agents/registry.py

--- agents/registry.py
+++
@@ -1,9 +0,0 @@
-from .research import ResearchAgent
-from .planning import PlanningAgent
-from .outreach import OutreachAgent
-
-AGENT_REGISTRY = {
-    "research": ResearchAgent,
-    "plan": PlanningAgent,
-    "outreach": OutreachAgent,
-}
